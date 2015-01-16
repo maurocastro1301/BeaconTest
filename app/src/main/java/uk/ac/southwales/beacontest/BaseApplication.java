8cafc58ec1220e71b59f662013912fb9ccb08f97
Neil Williams <neil.williams@southwales.ac.uk>
2015-01-16 14:05:14 +0000

    Initial Commit

---
+++ app/src/main/java/uk/ac/southwales/beacontest/BaseApplication.java
@@ -0,0 +1,22 @@
+package uk.ac.southwales.beacontest;
+
+import android.app.Application;
+
+import org.altbeacon.beacon.Region;
+import org.altbeacon.beacon.powersave.BackgroundPowerSaver;
+import org.altbeacon.beacon.startup.BootstrapNotifier;
+
+/**
+ * Created by nwillia2 on 16/01/2015.
+ */
+public class BaseApplication extends Application {
+    private BackgroundPowerSaver backgroundPowerSaver;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // Simply constructing this class and holding a reference to it in your custom Application class
+        // enables auto battery saving of about 60%
+        backgroundPowerSaver = new BackgroundPowerSaver(this);
+    }
+}
---
+++ app/src/main/java/uk/ac/southwales/beacontest/MainActivity.java
@@ -0,0 +1,58 @@
+package uk.ac.southwales.beacontest;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.BeaconTransmitter;
+
+import java.util.Arrays;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Beacon beacon = new Beacon.Builder()
+                .setId1("2f234454-cf6d-4a0f-adf2-f4911ba9ffa6")
+                .setId2("1")
+                .setId3("2")
+                .setManufacturer(0x0118)
+                .setTxPower(-59)
+                .setDataFields(Arrays.asList(new Long[]{0l}))
+                .build();
+        BeaconParser beaconParser = new BeaconParser()
+                .setBeaconLayout("m:2-3=beac,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25");
+        BeaconTransmitter beaconTransmitter = new BeaconTransmitter(getApplicationContext(), beaconParser);
+        beaconTransmitter.startAdvertising(beacon);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
